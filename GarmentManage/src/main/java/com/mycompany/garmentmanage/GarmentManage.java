6eb3c79bc0f37d140b54ba6276eb1b12bf331228
Rafia-Sultana-Mim <sultana23105101204@diu.edu.bd>
2024-11-20 22:55:18 +0600

    Test Commit

---
+++ GarmentManage/src/main/java/com/mycompany/garmentmanage/GarmentManage.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.garmentmanage;
+
+/**
+ *
+ * @author U S E R
+ */
+public class GarmentManage {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
